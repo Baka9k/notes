bb12ef25520d5827b81fe9875716ea4a8621dc5f
Baka <neoplanspaceliner9@gmail.com>
2016-11-03 18:22:34 +0300

    Add more tests, update README.md

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
-# notes
-In this application you can create, edit and delete notes, and use hashtags. App stores its data in localStorage.
+# Notes
+This application allows you to keep your notes organized.  
+* Create, delete, edit and view notes with ease.  
+* Use tags to organize your data.  
+* Filter items by entering the tags into search bar.  
 
-## How to test:
+## Development
+The application uses only HTML5 DOM API and ES6 JavaScript.    
+Notes are stored in HTML5 localStorage (if available).  
+Javascript code and styles are preprocessed and compiled with Webpack.  
+There is a set of end-to-end tests, mocha/chai and selenium-webdriver are used for testing.  
+
+## How to run the application on local machine
+Pre-built version is included in the repo, you can just clone the repo
+```
+git clone https://github.com/Baka9k/notes
+```
+Then, open `notes/index.html` in your browser, preferably chrome  
+
+## How to build
 ```
 git clone https://github.com/Baka9k/notes
 cd notes
-npm install
-
+npm install 
+npm run build
 ```
 
-
-
+## How to run end-to-end tests
+In notes directory, type this command and observe how chrome windows opens and runs full test suite
+```
+npm test
+```
--- test/test.js
+++ test/test.js
@@ -46,5 +46,157 @@ describe('Todolist web application functional test', function () {
             done();
         });
     });
+    
+    it('Appends item without tags', function (done) {
+		driver.findElement(By.css('div.list > div:nth-child(1) > .list-text'))
+		.sendKeys('lorem ipsum');
+		
+		driver.findElement(By.css('div.list > div:nth-child(1) > .action-create'))
+		.click();
+		
+		driver.findElements(By.css('.list-elem')).then( (elems) => {
+			expect(elems.length).to.equal(2);
+		});
+		
+		driver.findElement(By.css('div.list > div:nth-child(2) > .list-text')).getText().then( (text) => {
+		    expect(text).to.equal('lorem ipsum');
+			done();
+		});
+    });
+    
+    it('Deletes item without tags', function (done) {
+		driver.findElement(By.css('div.list > div:nth-child(2) > .action-remove'))
+		.click();
+		
+		driver.findElements(By.css('.list-elem')).then( (elems) => {
+			expect(elems.length).to.equal(1);
+			done();
+		});
+    });
+    
+    it('Appends item with tags', function (done) {
+		driver.findElement(By.css('div.list > div:nth-child(1) > .list-text'))
+		.sendKeys('lorem #test ipsum #javascript');
+		
+		driver.sleep(250);
+		
+		driver.findElement(By.css('div.list > div:nth-child(1) > .list-tags'))
+		.getText()
+		.then( (text) => {
+		    expect(text).to.equal('#test #javascript');
+		});
+		
+		driver.findElement(By.css('div.list > div:nth-child(1) > .action-create'))
+		.click();
+		
+		driver.findElements(By.css('.list-elem')).then( (elems) => {
+			expect(elems.length).to.equal(2);
+		});
+		
+		driver.findElement(By.css('div.list > div:nth-child(2) > .list-text')).getText().then( (text) => {
+		    expect(text).to.equal('lorem #test ipsum #javascript');
+		});
+		
+		driver.findElement(By.css('div.list > div:nth-child(2) > .list-tags')).getText().then( (text) => {
+		    expect(text).to.equal('#test #javascript');
+			done();
+		});
+    });
+    
+    it('Deletes item with tags', function (done) {
+		driver.findElement(By.css('div.list > div:nth-child(2) > .action-remove'))
+		.click();
+		
+		driver.findElements(By.css('.list-elem')).then( (elems) => {
+			expect(elems.length).to.equal(1);
+			done();
+		});
+    });
+    
+    it('Appends several items with tags', function (done) {
+		driver.findElement(By.css('div.list > div:nth-child(1) > .list-text'))
+		.sendKeys('lorem #test ipsum #javascript');
+		
+		driver.sleep(250);
+		
+		driver.findElement(By.css('div.list > div:nth-child(1) > .list-tags'))
+		.getText()
+		.then( (text) => {
+		    expect(text).to.equal('#test #javascript');
+		});
+		
+		driver.findElement(By.css('div.list > div:nth-child(1) > .action-create'))
+		.click();
+		
+		driver.sleep(100);
+		
+		driver.findElement(By.css('div.list > div:nth-child(1) > .list-text'))
+		.sendKeys('delorem #java #test rel');
+		
+		driver.sleep(250);
+		
+		driver.findElement(By.css('div.list > div:nth-child(1) > .list-tags'))
+		.getText()
+		.then( (text) => {
+		    expect(text).to.equal('#java #test');
+		});
+		
+		driver.findElement(By.css('div.list > div:nth-child(1) > .action-create'))
+		.click();
+		
+		driver.findElements(By.css('.list-elem')).then( (elems) => {
+			expect(elems.length).to.equal(3);
+			done();
+		});
+		
+    });
+    
+    it('Filters items with tags', function (done) {
+		driver.findElement(By.css('#filter-edit'))
+		.sendKeys('#test');
+		
+		driver.sleep(250);
+		
+		driver.findElements(By.css('.list-elem')).then( (elems) => {
+			expect(elems.length).to.equal(3);
+		});
+		
+		driver.findElement(By.css('#filter-edit'))
+		.sendKeys(' #java');
+		
+		driver.sleep(250);
+		
+		driver.findElement(By.css('div.list > div:nth-child(2)'))
+		.getAttribute('style').then( (css) => {
+			expect(css).to.equal('display: block;');
+		});
+		
+		driver.findElement(By.css('div.list > div:nth-child(2) > .list-tags'))
+		.getText()
+		.then( (text) => {
+		    expect(text).to.equal('#java #test');
+		});
+		
+		driver.findElement(By.css('div.list > div:nth-child(3)'))
+		.getAttribute('style').then( (css) => {
+			expect(css).to.equal('display: none;');
+		});
+		
+		driver.findElement(By.css('#clearb'))
+		.click();
+		
+		driver.sleep(250);
+		
+		driver.findElement(By.css('div.list > div:nth-child(3)'))
+		.getAttribute('style').then( (css) => {
+			expect(css).to.equal('display: block;');
+		});
+		
+		driver.findElement(By.css('div.list > div:nth-child(2)'))
+		.getAttribute('style').then( (css) => {
+			expect(css).to.equal('display: block;');
+			done();
+		});
+    });
 	
 });
